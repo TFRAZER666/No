68c51787fc0d8a7f85345cc1d2b28ed4a9e0ae37
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2019-05-30 17:30:42 +0800

    Create load_csv.R

---
+++ load_csv.R
@@ -0,0 +1,11 @@
+
+
+# Load the data. Make sure the path is correct for your computer 
+dat <- read.csv("crunchbase_monthly_export.csv")
+
+# have a look at what the data looks like
+head(dat)
+
+# Get the number of rows and columns of the data
+dim(dat)
+
