ecc6de4a95deb329760dc5554c94c6b77b213070
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2019-05-31 14:44:31 +0800

    Update load_csv.R

--- load_csv.R
+++ load_csv.R
@@ -1,4 +1,8 @@
 
+# Show the current working directory
+
+# Path to file on Sam's computer
+path_to_file <- "~/Documents/GitHub/No/crunchbase_monthly_export.csv"
 
 # Load the data. Make sure the path is correct for your computer 
 dat <- read.csv("crunchbase_monthly_export.csv")
